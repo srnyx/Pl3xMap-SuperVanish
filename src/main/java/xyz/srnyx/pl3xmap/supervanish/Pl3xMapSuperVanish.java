d21a35ce7347004e57d7e69bed130920b9aa5040
srnyx <25808801+srnyx@users.noreply.github.com>
2024-04-02 17:55:20 -0400

    Improve listener registration

--- src/main/java/xyz/srnyx/pl3xmap/supervanish/Pl3xMapSuperVanish.java
+++ src/main/java/xyz/srnyx/pl3xmap/supervanish/Pl3xMapSuperVanish.java
@@ -8,30 +8,36 @@ import net.pl3x.map.core.player.PlayerRegistry;
 
 import org.bukkit.Bukkit;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import xyz.srnyx.annoyingapi.AnnoyingPlugin;
 
 
 public class Pl3xMapSuperVanish extends AnnoyingPlugin {
+    @NotNull private final PlayerListener listener = new PlayerListener(this);
     @Nullable public FileVanishStateMgr vanishStateManager;
     @Nullable public PlayerRegistry mapPlayerRegistry;
 
     public Pl3xMapSuperVanish() {
-        options
-                .bStatsOptions(bStatsOptions -> bStatsOptions.id(21336))
-                .registrationOptions.toRegister(new PlayerListener(this));
+        options.bStatsOptions(bStatsOptions -> bStatsOptions.id(21336));
     }
 
     @Override
     public void enable() {
+        reload();
+    }
+
+    @Override
+    public void reload() {
         final SuperVanish superVanish = (SuperVanish) Bukkit.getPluginManager().getPlugin("SuperVanish");
-        if (superVanish == null) {
-            getLogger().severe("SuperVanish not found! Disabling...");
-            Bukkit.getPluginManager().disablePlugin(this);
+        if (superVanish == null) { // This should never happen, but just in case
+            LOGGER.severe("SuperVanish not found!");
+            listener.unregister();
             return;
         }
         vanishStateManager = superVanish.getVanishStateMgr();
         mapPlayerRegistry = Pl3xMap.api().getPlayerRegistry();
+        listener.register();
     }
 }
